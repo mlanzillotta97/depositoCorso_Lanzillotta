0876b39d7828377d782e74e9039897f52d2f3654
mlanzillotta97 <129388713+mlanzillotta97@users.noreply.github.com>
2026-02-16 17:53:39 +0100

    primo commit

---
+++ Corso C#/lunedi_16_Febbraio/mattina/progetto_helloWorld/progetto_helloWorld/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+
+//questo è un commento 
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        int contatore; //dichiarazione 
+        contatore = 0; //assegnazione 
+        int somma = 10; //dichiarazione + inizializzazione
+        var nome = "Ada"; //tipo dedotto dal compilatore 
+        int numero = 10; //numero intero 
+        float decimaleFloat = 3.14f; //numero a virgola mobile
+        double decimaleDouble = 3.14d; //numero a virgola mobile 
+        char lettera = 'A'; //singolo carattere 
+        bool condizione = true; //bit 0 (false) o 1 (true) 
+        string saluto = "Ciao a tutti!"; //molteplici caratteri 
+        const float PI_NUMBER = 3.14159265359f; 
+
+        Console.WriteLine(contatore);
+        Console.WriteLine(0);
+        Console.WriteLine(somma);
+        Console.WriteLine(nome);
+        Console.WriteLine("Numero: " + numero);
+        Console.WriteLine("DecimaleFloat: " + decimaleFloat);
+        Console.WriteLine("DecimaleDouble: " + decimaleDouble);
+        Console.WriteLine("Lettera: " + lettera);
+        Console.WriteLine("Condizione: " + condizione);
+        Console.WriteLine("Saluto: " + saluto);
+        Console.WriteLine(PI_NUMBER);
+        */
+
+        /*
+        int intero = 42; 
+        float numeroLungo = intero;
+
+        float pi = 3.14f; 
+        int circaPi = (int)pi;
+
+        Console.WriteLine(numeroLungo);
+        Console.WriteLine(circaPi);
+        */
+        /*
+        Console.Write("Come ti chiami?");
+        string nome = Console.ReadLine();
+        Console.Write($"Ciao {nome}!");
+
+        Console.Write("Dammi un numero?");
+        string n1 = Console.ReadLine();
+        Console.Write("Dammi un altro numero? ");
+        string n2 = Console.ReadLine();
+
+        int numero1 = int.Parse(n1);
+        int numero2 = int.Parse(n2);
+
+        Console.WriteLine($"Somma {numero1 + numero2}");
+        */
+
+        
+        /*
+        int a = 1;
+        int b = 10;
+
+        Console.WriteLine("Operatori aritmetici: ");
+        Console.WriteLine($"a = {a}, b = {b}");
+        Console.WriteLine($"Addizione: a + b = {a + b}");
+        Console.WriteLine($"Sottrazione: a - b = {a - b}");
+        Console.WriteLine($"Moltiplicazione: a - b = {a * b}");
+        Console.WriteLine($"Divisione: a - b = {a / b}");
+        Console.WriteLine($"Modulo (resto): a % b = {a / b}");
+        Console.WriteLine("Operatori Assegnamento:");
+        Console.WriteLine($"Incremento: a++ = {a++}");
+        Console.WriteLine($"Ora a = {a}");
+        Console.WriteLine($"Decremento: --b = {--b}");
+        Console.WriteLine($"Ora b = {b}");
+        */
+
+        int a = 10;
+        int b = 15; 
+
+        Console.WriteLine("Operatori Logici:");
+        Console.WriteLine($"AND: {a < b && a >=10}");
+        Console.WriteLine($"OR: {a < b || a >=10}");
+        Console.WriteLine($"NOT: {!(a < b)}");
+        }
+        //public readonly string Nome = "Franco";
+        
+}
+
+
+
